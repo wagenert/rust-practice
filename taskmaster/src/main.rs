207aa89796551360c2687aa64bd0c7a20ae21519
Thomas Wagener <thomas.wagener@fischantiquariat.de>
2026-06-27 18:06:58 +0200

    First implementation with static

--- taskmaster/src/main.rs
+++ taskmaster/src/main.rs
@@ -1,3 +1,55 @@
+mod task;
+
+use clap::{Parser, Subcommand};
+use task::TaskId;
+
+use crate::task::Task;
+
+#[derive(Parser)]
+struct Cli {
+    /// Command to execute
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Clone, Subcommand)]
+enum Command {
+    /// List all tasks
+    List,
+    /// Create new tasks
+    Create {
+        /// Title of the new task
+        title: String,
+    },
+    /// Mark a task as done
+    MarkDone {
+        /// task_id to mark as done
+        task_id: TaskId,
+    },
+}
+
+static mut CURRENT_ID: TaskId = 0;
+static mut TASKS: Vec<Task> = vec![];
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    match cli.command {
+        Command::List => {
+            println!("Tasks");
+            for task in unsafe { TASKS.clone() } {
+                println!("{task}");
+            }
+        }
+        Command::Create { title } => {
+            let task_id = unsafe { CURRENT_ID };
+            unsafe { CURRENT_ID += 1 };
+            let new_task = Task::new(task_id, title);
+            unsafe { TASKS.push(new_task) };
+        }
+        Command::MarkDone { task_id } => {
+            if let Some(task) = unsafe { TASKS.get_mut(task_id as usize) } {
+                task.done();
+            }
+        }
+    }
 }
---
+++ taskmaster/src/task.rs
@@ -0,0 +1,34 @@
+use std::fmt;
+
+#[derive(Debug, Clone)]
+pub struct Task {
+    id: TaskId,
+    title: String,
+    done: bool,
+}
+
+impl Task {
+    pub fn new(id: TaskId, title: String) -> Self {
+        Self {
+            id,
+            title,
+            done: false,
+        }
+    }
+
+    pub fn done(&mut self) {
+        self.done = true;
+    }
+}
+
+impl fmt::Display for Task {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "Task {{ id: {}, title: {}, done: {} }}",
+            self.id, self.title, self.done
+        )
+    }
+}
+
+pub type TaskId = u32;
