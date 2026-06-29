cec5ea02b72c9ae2f61c24bac4d940ad0c63b76f
Thomas Wagener <thomas.wagener@gls-group.com>
2026-06-29 10:02:17 +0200

    Moved file logic to subcommands. Still with issues on updating.

--- taskmaster/src/main.rs
+++ taskmaster/src/main.rs
@@ -38,31 +38,43 @@ enum Command {
 
 fn main() -> std::io::Result<()> {
     let cli = Cli::parse();
-    let file = OpenOptions::new()
-        .read(true)
-        .write(true)
-        .create(true)
-        .truncate(true)
-        .open(cli.filename)?;
-    let read_buf = BufReader::new(&file);
-    let mut tasks: Vec<Task> = match serde_json::from_reader(read_buf) {
-        Ok(tasks) => tasks,
-        Err(err) => {
-            if err.is_eof() {
-                Vec::new()
-            } else {
-                panic!("Can not process file!");
-            }
-        }
-    };
     match cli.command {
         Command::List => {
-            println!("Tasks");
-            for task in tasks.iter() {
-                println!("{task}");
+            let filepath = std::path::Path::new(&cli.filename);
+            if filepath.exists() {
+                let file = OpenOptions::new()
+                    .read(true)
+                    .create(false)
+                    .open(cli.filename)?;
+                let read_buf = BufReader::new(&file);
+                let tasks: Vec<Task> = serde_json::from_reader(read_buf)?;
+                println!("Tasks");
+                for task in tasks.iter() {
+                    println!("{task}");
+                }
+            } else {
+                println!("File does not exist. No tasks found.");
             }
         }
         Command::Create { title } => {
+            let file = OpenOptions::new()
+                .read(true)
+                .write(true)
+                .create(true)
+                .truncate(true)
+                .open(cli.filename)?;
+            let read_buf = BufReader::new(&file);
+            let mut tasks: Vec<Task> = match serde_json::from_reader(read_buf) {
+                Ok(tasks) => tasks,
+                Err(err) => {
+                    if err.is_eof() {
+                        Vec::new()
+                    } else {
+                        panic!("Can not process file!");
+                    }
+                }
+            };
+
             let task_id = tasks.len();
             let new_task = Task::new(task_id as TaskId, title);
             tasks.push(new_task);
@@ -70,11 +82,31 @@ fn main() -> std::io::Result<()> {
             serde_json::to_writer(write_buf, &tasks)?;
         }
         Command::MarkDone { task_id } => {
+            let file = OpenOptions::new()
+                .read(true)
+                .write(true)
+                .create(false)
+                .truncate(true)
+                .open(cli.filename)?;
+            let read_buf = BufReader::new(&file);
+            let mut tasks: Vec<Task> = match serde_json::from_reader(read_buf) {
+                Ok(tasks) => tasks,
+                Err(err) => {
+                    if err.is_eof() {
+                        Vec::new()
+                    } else {
+                        panic!("Can not process file!");
+                    }
+                }
+            };
+
             if let Some(task) = tasks.get_mut(task_id as usize) {
                 task.done();
-                let write_buf = BufWriter::new(file);
-                serde_json::to_writer(write_buf, &tasks)?;
+            } else {
+                println!("Task with id {task_id} not found.");
             }
+            let write_buf = BufWriter::new(file);
+            serde_json::to_writer(write_buf, &tasks)?;
         }
     }
     Ok(())
